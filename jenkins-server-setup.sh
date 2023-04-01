a3d88131ce56c9aedafc941fd1188bdfd4116edb
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2023-04-01 21:10:11 +0530

    Create jenkins-server-setup.sh

---
+++ jenkins-server-setup.sh
@@ -0,0 +1,9 @@
+yum install wget -y
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+  
+yum install fontconfig java-11-openjdk -y
+
+yum install jenkins  -y
