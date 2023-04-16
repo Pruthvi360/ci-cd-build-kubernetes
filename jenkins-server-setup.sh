c6eef6034e955ecbaa485d2e17cc9846a2640915
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2023-04-16 12:58:51 +0530

    Update jenkins-server-setup.sh

--- jenkins-server-setup.sh
+++ jenkins-server-setup.sh
@@ -1,11 +1,13 @@
 yum install wget -y
 
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat/jenkins.repo
+    
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
 
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-  
-yum install fontconfig java-11-openjdk -y
+sudo yum upgrade
 
-yum install jenkins  -y
+# Add required dependencies for the jenkins package
+sudo yum install java-11-openjdk
 
-systemctl start jenkins
+sudo yum install jenkins
