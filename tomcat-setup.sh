04bd99f9bfb3f785b5b51bbe0838f740262bcbbb
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2023-04-18 15:49:35 +0530

    Update tomcat-setup.sh

--- tomcat-setup.sh
+++ tomcat-setup.sh
@@ -11,6 +11,7 @@ yum install wget -y
 
 cd /opt
 wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.7/bin/apache-tomcat-10.1.7.tar.gz
+tar -xvzf /opt/apache-tomcat-10.1.7.tar.gz
 
 ## change execution permission
 
