043f7b16e9ccf09da635205aefd61502a01d4392
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2023-04-18 20:32:34 +0530

    Update tomcat-setup.sh

--- tomcat-setup.sh
+++ tomcat-setup.sh
@@ -47,14 +47,15 @@ sed -i '49,56d' /opt/apache-tomcat-10.1.7/conf/tomcat-users.xml
 
 ## Insert users
 
-echo "<role rolename="manager-gui"/>
-<role rolename="manager-script"/>
-<role rolename="manager-jmx"/>
-<role rolename="manager-status"/>
-<user username="admin" password="admin" roles="manager-gui, manager-script, manager-jmx, manager-status"/>
-<user username="deployer" password="deployer" roles="manager-script"/>
-<user username="tomcat" password="s3cret" roles="manager-gui"/>
-</tomcat-users>" >> /opt/apache-tomcat-10.1.7/conf/tomcat-users.xml
+echo "
+  <role rolename="manager-gui"/>
+  <role rolename="manager-script"/>
+  <role rolename="manager-jmx"/>
+  <role rolename="manager-status"/>
+  <user username="admin" password="admin" roles="manager-gui, manager-script, manager-jmx, manager-status"/>
+  <user username="deployer" password="deployer" roles="manager-script"/>
+  <user username="tomcat" password="s3cret" roles="manager-gui"/>
+  </tomcat-users>" >> /opt/apache-tomcat-10.1.7/conf/tomcat-users.xml
 
 
 ## Starting tomcatserver
