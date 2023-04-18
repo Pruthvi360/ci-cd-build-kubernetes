6b8573fb4d09ee282acbfb0d725d1325f5eab9ba
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2023-04-18 15:39:16 +0530

    Update tomcat-setup.sh

--- tomcat-setup.sh
+++ tomcat-setup.sh
@@ -2,7 +2,7 @@
 
 # Create tomcat directory
 cd /opt
-wget [http://mirrors.fibergrid.in/apache/tomcat/tomcat-8/v8.5.35/bin/apache-tomcat-8.5.35.tar.gz](https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.7/bin/apache-tomcat-10.1.7.tar.gz)
+wget http://mirrors.fibergrid.in/apache/tomcat/tomcat-8/v8.5.35/bin/apache-tomcat-8.5.35.tar.gz](https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.7/bin/apache-tomcat-10.1.7.tar.gz
 tar -xvzf /opt/apache-tomcat-10.1.7.tar.gz
 
 ## change execution permission
