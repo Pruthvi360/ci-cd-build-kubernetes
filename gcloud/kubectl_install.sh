eb70594281985b539e10341d7f9a0571c2344879
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2023-05-05 21:16:47 +0530

    Create kubectl_install.sh

---
+++ gcloud/kubectl_install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+## kubernetes configuration 
+
+gcloud components install kubectl
+
+gcloud components install gke-gcloud-auth-plugin
+
+sudo apt-get install google-cloud-cli-gke-gcloud-auth-plugin
