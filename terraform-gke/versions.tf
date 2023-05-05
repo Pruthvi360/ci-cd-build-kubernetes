9d6401331f82357cc5f0e0f766fd3ad29c2cf162
Pruthvi_cloud_deveops <107435692+Pruthvi360@users.noreply.github.com>
2023-05-05 21:53:51 +0530

    Update versions.tf

--- terraform-gke/versions.tf
+++ terraform-gke/versions.tf
@@ -1,14 +1,11 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "4.27.0"
+      credentials = file("service-account.json")
     }
   }
 
   required_version = ">= 0.14"
 }
-
